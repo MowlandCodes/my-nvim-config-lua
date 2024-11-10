af987fea5fa710ce57bb324d24fb97a50a701b35
MowlandCodes <mowlandgaming@gmail.com>
2024-11-11 00:41:21 +0700

    keymap set to silent

--- lua/config/lazy_nvim.lua
+++ lua/config/lazy_nvim.lua
@@ -34,5 +34,5 @@ require("lazy").setup({
     checker = { enabled = true },
 })
 
-vim.keymap.set('n', '<leader>lz', ':Lazy<CR>', {})
+vim.keymap.set('n', '<leader>lz', ':Lazy<CR>', { silent = true })
 ------------------------------------------------------------------------------------------------------------------
