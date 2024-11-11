1b65e59e2f9632eb0bf6818e6eee75cb237c2c33
MowlandCodes <mowlandgaming@gmail.com>
2024-11-11 23:37:25 +0700

    added telescope-cmdline

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -7,7 +7,7 @@ return {
         },
     },
     {
-        "hrsh7th/cmp-nvim-lsp"
+        "hrsh7th/cmp-nvim-lsp",
     },
     {
         "hrsh7th/nvim-cmp",
--- lua/plugins/nvim_telescope.lua
+++ lua/plugins/nvim_telescope.lua
@@ -1,26 +1,48 @@
 return {
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        config = function()
-            -- Using Telescope Package
-            local builtin = require("telescope.builtin")
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Telescope - Find Files" })
-            vim.keymap.set('n', '<leader>lg', builtin.live_grep, { desc = "Telescope - Live Greps" })
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Telescope - Buffers" })
-        end
-    },
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            require("telescope").setup{
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown{}
-                    }
-                }
-            }
-            require("telescope").load_extension("ui-select")
-        end
-    },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"jonarrien/telescope-cmdline.nvim",
+		},
+		config = function()
+            vim.keymap.set("n", "<leader><leader>", ":Telescope cmdline<CR>", { silent = true })
+			-- Using Telescope Package
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope - Find Files" })
+			vim.keymap.set("n", "<leader>lg", builtin.live_grep, { desc = "Telescope - Live Greps" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope - Buffers" })
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+					["cmdline"] = {
+						picker = {
+							layout_config = {
+								width = 120,
+								height = 25,
+							},
+						},
+					},
+					mappings = {
+						complete = "<Tab>",
+						run_selection = "<C-CR>",
+						run_input = "<CR>",
+					},
+					overseer = {
+						enabled = true,
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+			require("telescope").load_extension("cmdline")
+		end,
+	},
 }
