378f98fa1d5d022542cb41ffdc9d89542ff091a0
MowlandCodes <mowlandgaming@gmail.com>
2024-11-12 23:30:55 +0700

    allowing Codeium to spawn Chat in browser

--- lua/plugins/ai_integrations.lua
+++ lua/plugins/ai_integrations.lua
@@ -1,11 +1,13 @@
 return {
-    "Exafunction/codeium.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "hrsh7th/nvim-cmp",
-    },
-    config = function()
-        require("codeium").setup({})
-    end,
-}
+	"Exafunction/codeium.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"hrsh7th/nvim-cmp",
+	},
+	config = function()
+		require("codeium").setup({})
 
+		-- Spawning Codeium Chat in Browser
+		vim.keymap.set("n", "<leader>aa", ":Codeium Chat<CR>", { silent = true })
+	end,
+}
