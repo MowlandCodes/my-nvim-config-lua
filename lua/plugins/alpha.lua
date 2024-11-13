f631acf252d2fb5882189a86cf0e5d360307c02c
MowlandCodes <mowlandgaming@gmail.com>
2024-11-13 20:31:20 +0700

    changed the Alpha Greeter in Intro

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -3,11 +3,19 @@ return {
     -- dependencies = { 'echasnovski/mini.icons' },
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+        local dashboard = require("alpha")
         local startify = require("alpha.themes.startify")
         -- available: devicons, mini, default is mini
-        -- if provider not loaded and enabled is true, it will try to use another provider
         startify.file_icons.provider = "devicons"
-        require("alpha").setup(
+        startify.section.header.val = {
+           [[ ███████╗ █████╗ ██████╗ ███╗   ███╗ ██████╗ ███████╗██╗]],
+           [[ ██╔════╝██╔══██╗██╔══██╗████╗ ████║██╔═══██╗██╔════╝██║]],
+           [[ █████╗  ███████║██████╔╝██╔████╔██║██║   ██║█████╗  ██║]],
+           [[ ██╔══╝  ██╔══██║██╔══██╗██║╚██╔╝██║██║   ██║██╔══╝  ██║]],
+           [[ ██║     ██║  ██║██║  ██║██║ ╚═╝ ██║╚██████╔╝███████╗███████╗]],
+           [[ ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚══════╝]],
+        }
+        dashboard.setup(
             startify.config
         )
     end,
