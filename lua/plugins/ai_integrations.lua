f020397d411dd1f1485e7705a0fda8c8739a5aa2
MowlandCodes <mowlandgaming@gmail.com>
2024-11-15 01:30:52 +0700

    added Ollama Integration (Experimental)

--- lua/plugins/ai_integrations.lua
+++ lua/plugins/ai_integrations.lua
@@ -1,13 +1,39 @@
 return {
-	"Exafunction/codeium.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"hrsh7th/nvim-cmp",
-	},
-	config = function()
-		require("codeium").setup({})
+    {
+        "Exafunction/codeium.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "hrsh7th/nvim-cmp",
+        },
+        config = function()
+            require("codeium").setup({})
 
-		-- Spawning Codeium Chat in Browser
-		vim.keymap.set("n", "<leader>aa", ":Codeium Chat<CR>", { silent = true })
-	end,
+            -- Spawning Codeium Chat in Browser (using formatted string)
+            vim.api.nvim_buf_set_keymap(0, "n", "<Leader>aa", ":Codeium Chat<CR>", { silent = true })
+        end,
+    },
+    {
+        "David-Kunz/gen.nvim",
+        config = function()
+            -- Import the gen plugin
+            local gen = require("gen")
+
+            -- Configure the gen plugin
+            gen.setup({
+                -- Display the model when it is being used
+                show_model = true,
+
+                -- Display the prompt when it is being used
+                show_prompt = true,
+
+                -- Display the generated code in a split window
+                display_mode = "float",
+                no_auto_close = true,
+            })
+
+            vim.keymap.set({ "n", "v" }, '<leader>al', ":Gen<CR>")
+            vim.keymap.set({ "n", "v" }, '<leader>ac', ":Gen Chat<CR>")
+
+        end,
+    },
 }
