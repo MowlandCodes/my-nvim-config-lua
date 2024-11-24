b1fc2d5fc1ea4ae1af1c260639279e07f3b49b29
MowlandCodes <mowlandgaming@gmail.com>
2024-11-24 15:25:24 +0700

    added Flutter Support for Neovim

---
+++ lua/plugins/flutter-plugin.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        "stevearc/dressing.nvim",
+        lazy = false,
+        opts = {}
+    },
+    {
+        "akinsho/flutter-tools.nvim",
+        lazy = false,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "stevearc/dressing.nvim",
+        },
+        config = function()
+            require("flutter-tools").setup()
+        end,
+    },
+}
