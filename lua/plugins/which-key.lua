f59f5dfa5b4a71c5efc1acd7dba3564e784b82a4
mowlandcodes <mowlandgaming@gmail.com>
2024-12-25 15:28:35 +0700

    added keybinding to which-key for window managing

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,7 +2,8 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     config = function()
-        require("which-key").setup({
+        local wk = require("which-key")
+        wk.setup({
             plugins = {
                 marks = true,
                 registers = true,
@@ -20,6 +21,9 @@ return {
                     g = true,
                 },
             },
+        })
+        wk.add({
+            { "<leader>w", proxy = "<c-w>", group = "windows" }
         })
     end,
 }
