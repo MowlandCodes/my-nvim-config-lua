30a296ea5db2de28ce39686a8ed89982f6e7be23
mowlandcodes <mowlandgaming@gmail.com>
2024-12-25 12:19:47 +0700

    added % at autoclose plugin

--- lua/plugins/autoclose.lua
+++ lua/plugins/autoclose.lua
@@ -1,10 +1,26 @@
 return {
-	"m4xshen/autoclose.nvim",
-	config = function()
-		require("autoclose").setup({
-			options = {
-				pair_spaces = true,
-			},
-		})
-	end,
+    "m4xshen/autoclose.nvim",
+    config = function()
+        require("autoclose").setup({
+            options = {
+                pair_spaces = true,
+                disabled_when_touch = false,
+            },
+            keys = {
+                ["%"] = { escape = true, close = true, pair = "%%", disabled_filetypes = {} },
+                ["("] = { escape = false, close = true, pair = "()", disabled_filetypes = {} },
+                ["["] = { escape = false, close = true, pair = "[]", disabled_filetypes = {} },
+                ["{"] = { escape = false, close = true, pair = "{}", disabled_filetypes = {} },
+
+                [">"] = { escape = true, close = false, pair = "<>", disabled_filetypes = {} },
+                [")"] = { escape = true, close = false, pair = "()", disabled_filetypes = {} },
+                ["]"] = { escape = true, close = false, pair = "[]", disabled_filetypes = {} },
+                ["}"] = { escape = true, close = false, pair = "{}", disabled_filetypes = {} },
+
+                ['"'] = { escape = true, close = true, pair = '""', disabled_filetypes = {} },
+                ["'"] = { escape = true, close = true, pair = "''", disabled_filetypes = {} },
+                ["`"] = { escape = true, close = true, pair = "``", disabled_filetypes = {} },
+            },
+        })
+    end,
 }
