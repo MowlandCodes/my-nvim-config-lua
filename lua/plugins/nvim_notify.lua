ff44d84af6751e88c5832d444d74cebe17ba4a68
mowlandcodes <mowlandgaming@gmail.com>
2025-03-14 22:18:17 +0700

    add: added overseer for Task Management in Neovim. edit: removed autopairing percent sign (%) in files

--- lua/plugins/nvim_notify.lua
+++ lua/plugins/nvim_notify.lua
@@ -1,47 +1,47 @@
 return {
-    "rcarriga/nvim-notify",
-    config = function()
-        local notify = require("notify")
-        vim.opt.termguicolors = true
-        vim.notify = notify
-        vim.opt.showcmd = false
-        vim.opt.cmdheight = 0
-        vim.opt.laststatus = 2
+	"rcarriga/nvim-notify",
+	config = function()
+		local notify = require("notify")
+		vim.opt.termguicolors = true
+		vim.notify = notify
+		vim.opt.showcmd = false
+		vim.opt.cmdheight = 0
+		vim.opt.laststatus = 2
 
-        vim.api.nvim_create_autocmd('BufWritePost', {
-            callback = function()
-                local filename = vim.fn.expand('%:t') -- Get the full file path
-                require('notify')('File saved successfully: ' .. filename, 'info', { title = 'Save Notification' })
-            end,
-        })
+		vim.api.nvim_create_autocmd("BufWritePost", {
+			callback = function()
+				local filename = vim.fn.expand("%:t") -- Get the full file path
+				notify("File saved successfully: " .. filename, "info", { title = "Save Notification" })
+			end,
+		})
 
-        local function override_echo()
-            local original_echo = vim.api.nvim_echo
-            vim.api.nvim_echo = function(messages, _, _)
-                for _, message in ipairs(messages) do
-                    notify(message[1], 'info', { title = 'Message' })
-                end
-            end
-        end
+		local function override_echo()
+			local _ = vim.api.nvim_echo
+			vim.api.nvim_echo = function(messages, _, _)
+				for _, message in ipairs(messages) do
+					notify(message[1], "info", { title = "Message" })
+				end
+			end
+		end
 
-        local function override_out_write()
-            local original_out_write = vim.api.nvim_out_write
-            vim.api.nvim_out_write = function(msg)
-                notify(msg, 'info', { title = 'Output' })
-            end
-        end
+		local function override_out_write()
+			local _ = vim.api.nvim_out_write
+			vim.api.nvim_out_write = function(msg)
+				notify(msg, "info", { title = "Output" })
+			end
+		end
 
-        override_echo()
-        override_out_write()
+		override_echo()
+		override_out_write()
 
-        notify.setup({
-            background_colour = "#181926",
-            fps = 60,
-            render = "default",
-            stages = "slide",
-            timeout = 1700,
-            max_height = 10,
-            max_width = 80,
-        })
-    end
+		notify.setup({
+			background_colour = "#181926",
+			fps = 60,
+			render = "default",
+			stages = "slide",
+			timeout = 1700,
+			max_height = 10,
+			max_width = 80,
+		})
+	end,
 }
